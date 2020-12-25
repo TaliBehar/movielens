3b3fa45d69e9b3543c607480de533c3f29379db5
Tali Behar <talireuven@gmail.com>
2020-12-25 11:17:19 -0800

    Use a shortcut to load the data from local computer

--- MovieLens Project Fixed.R
+++ MovieLens Project Fixed.R
@@ -34,38 +34,42 @@ library(cowplot)
 
 ### 3.Create Edx set (training set) and Validation Sets (final hold-out test set)
 
-dl <- tempfile()
-download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+#### Use a shortcut to load the data from local computer
+load("D:/OneDrive/Documents/Tali/Tali data science studies/movielens_dataset/movielens_dataset_fixed.RData")
+####
 
-ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
-                 col.names = c("userId", "movieId", "rating", "timestamp"))
+#dl <- tempfile()
+#download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
 
-movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
-colnames(movies) <- c("movieId", "title", "genres")
+#ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+#                 col.names = c("userId", "movieId", "rating", "timestamp"))
+
+#movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+#colnames(movies) <- c("movieId", "title", "genres")
 
 # if using R 3.6 or earlier:
-movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))[movieId],
-                                           title = as.character(title),
-                                           genres = as.character(genres))
+#movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))[movieId],
+#                                          title = as.character(title),
+#                                         genres = as.character(genres))
 
-movielens <- left_join(ratings, movies, by = "movieId")
+#movielens <- left_join(ratings, movies, by = "movieId")
 
 # Validation set will be 10% of MovieLens data
-set.seed(1, sample.kind="Rounding") # if using R 3.5 or earlier, use `set.seed(1)`
-test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
-edx <- movielens[-test_index,]
-temp <- movielens[test_index,]
+#set.seed(1, sample.kind="Rounding") # if using R 3.5 or earlier, use `set.seed(1)`
+#test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+#edx <- movielens[-test_index,]
+#temp <- movielens[test_index,]
 
 # Make sure userId and movieId in validation set are also in edx set
-validation <- temp %>% 
-  semi_join(edx, by = "movieId") %>%
-  semi_join(edx, by = "userId")
+#validation <- temp %>% 
+#  semi_join(edx, by = "movieId") %>%
+#  semi_join(edx, by = "userId")
 
 # Add rows removed from validation set back into edx set
-removed <- anti_join(temp, validation)
-edx <- rbind(edx, removed)
+#removed <- anti_join(temp, validation)
+#edx <- rbind(edx, removed)
 
-rm(dl, ratings, movies, test_index, temp, movielens, removed)
+#rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
 ### 4. sanitizing the data - timestamp column
 # edx data set contains a timestamp column.This date-time is a point on the timeline, stored as the number
@@ -73,9 +77,9 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 # convert timestamp into rate date, add rate year column, extract the release year from the movie title, 
 # delete release year from movie title
 
-edx_year_sanitized <- edx %>% 
-  mutate(rate_year = year(as_datetime(timestamp)),
-          rate_date = date(as_datetime(timestamp)),
-          release_year =as.numeric(str_extract(title, "(?<=\\()(\\d{4})(?=\\))")),
-          title= str_remove(as.character(title),"(\\(\\d{4}\\))")) %>% 
-  select(-timestamp)
+#edx_year_sanitized <- edx %>% 
+#  mutate(rate_year = year(as_datetime(timestamp)),
+#          rate_date = date(as_datetime(timestamp)),
+#          release_year =as.numeric(str_extract(title, "(?<=\\()(\\d{4})(?=\\))")),
+#          title= str_remove(as.character(title),"(\\(\\d{4}\\))")) %>% 
+#  select(-timestamp)
